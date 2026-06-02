a3fd225d1c64544f2598dcfc90db43f896a4f2e1
withamrendra-rgb <withamrendra@gmail.com>
2026-06-02 07:49:20 +0530

    updatev2

--- azurerm_container/cntr-main.tf
+++ azurerm_container/cntr-main.tf
@@ -4,3 +4,12 @@ resource "azurerm_storage_container" "cntrproj1"{
     storage_account_name = each.value.storage_account_name
     container_access_type = each.value.container_access_type
 }
+
+terraform{
+    backend "azurerm"{
+        resource_group_name = "rg-terraform"
+        storage_account_name = "stterraform"
+        container_name = "tfstate"
+        key = "terraform.tfstate"
+    }
+}
